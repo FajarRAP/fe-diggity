4528c97c9ffa2956f2157fb20020f36a7602ea26
Fajar Riansyah <fajary781@gmail.com>
2024-12-15 22:06:18 +0700

    refactor (primary outline button): add outline transition

--- src/components/buttons/PrimaryOutlineButton.vue
+++ src/components/buttons/PrimaryOutlineButton.vue
@@ -5,7 +5,8 @@ defineProps({ text: String })
 </script>
 
 <template>
-  <button class="border rounded border-primary bg-onPrimary xl:py-3 lg:py-2 xl:px-5 lg:px-3 text-primary">
+  <button
+    class="transition-all border rounded outline-none border-primary bg-onPrimary xl:py-3 lg:py-2 xl:px-5 lg:px-3 text-primary focus:outline-primary focus:outline-offset-2">
     <HeadingSix :text="text" />
   </button>
 </template>
