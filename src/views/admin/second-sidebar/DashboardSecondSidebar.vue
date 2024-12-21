221ec1e5821a3093f35b72283bab3fd3b6e29a18
Fajar Riansyah <fajary781@gmail.com>
2024-12-21 18:51:35 +0700

    feat: add dashboard second sidebar component

---
+++ src/views/admin/second-sidebar/DashboardSecondSidebar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center xl:px-7 lg:px-5 xl:py-12 lg:py-8 xl:gap-12 lg:gap-8">
+    <img src="@/assets/icons/right.png" alt="right" class="mt-auto xl:size-6 lg:size-4 hover:cursor-pointer">
+  </div>
+  <RouterView />
+</template>
