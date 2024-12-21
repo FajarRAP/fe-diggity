b2559edd26519ecd7a366ccac63032c77fcdf6ff
Fajar Riansyah <fajary781@gmail.com>
2024-12-21 18:38:54 +0700

    feat: add warning button component

---
+++ src/components/buttons/WarningButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HeadingSix from '../fonts/HeadingSix.vue'
+
+defineProps({ text: String })
+</script>
+
+<template>
+  <button
+    class="px-3 py-2 transition-all rounded outline-none bg-warning text-onPrimary focus:outline-warning focus:outline focus:outline-offset-2 disabled:bg-gray-400">
+    <HeadingSix :text="text" />
+  </button>
+</template>
