c6b2bb9ea93370a175d619dabe2442038f335550
Fajar Riansyah <fajary781@gmail.com>
2024-12-25 21:28:48 +0700

    feat: add text field component

---
+++ src/components/fields/TextField.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { type InputTypeHTMLAttribute } from 'vue';
+import HeadingSix from '../fonts/HeadingSix.vue';
+
+const props = defineProps<{
+  id: string,
+  label: string,
+  placeholder?: string,
+  type?: InputTypeHTMLAttribute,
+  readonly?: boolean
+}>()
+
+const model = defineModel()
+</script>
+
+<template>
+  <div>
+    <label :for="props.id">
+      <HeadingSix :text="props.label" class="mb-1.5" />
+    </label>
+    <input :readonly="readonly" :type="props.type ?? 'text'"
+      class="w-full px-3 py-2 border border-gray-300 rounded bg-gray-50" :id="props.id"
+      :placeholder="props.placeholder ?? props.label" v-model="model">
+  </div>
+</template>
