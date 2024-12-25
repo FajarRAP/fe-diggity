aad686f95c1e3829d8962e061193e5c3f5e3ad27
Fajar Riansyah <fajary781@gmail.com>
2024-12-25 21:23:49 +0700

    feat: add service sidebar

--- src/views/admin/second-sidebar/ServiceSecondSidebar.vue
+++ src/views/admin/second-sidebar/ServiceSecondSidebar.vue
@@ -10,12 +10,12 @@ const isOpen = ref(false)
 <template>
   <WrapperSecondSidebar :class="{ 'min-w-40': isOpen }">
     <RouterLink :to="`${RoutesName.dashboardAdminRoute}/service/message`" active-class="text-primary bg-primary/20"
-      class="gap-3 p-2.5  rounded-lg w-full hover:cursor-pointer " v-if="isOpen">
+      class="gap-3 p-2.5 rounded-lg w-full hover:cursor-pointer " v-if="isOpen">
       <HeadingSix class="text-center" text="Pesan" />
     </RouterLink>
     <RouterLink :to="`${RoutesName.dashboardAdminRoute}/service/portfolio`" active-class="text-primary bg-primary/20"
-      class="gap-3 p-2.5  rounded-lg w-full hover:cursor-pointer " v-if="isOpen">
-      <HeadingSix class="text-center" text="Portfolio" />
+      class="gap-3 p-2.5 rounded-lg w-full hover:cursor-pointer " v-if="isOpen">
+      <HeadingSix class="text-center" text="Portofolio" />
     </RouterLink>
     <img src="@/assets/icons/right.png" alt="right" class="mt-auto xl:size-6 lg:size-4 me-auto hover:cursor-pointer"
       @click="isOpen = !isOpen">
