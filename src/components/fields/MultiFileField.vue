4e6664ec9a8bb5b5025e2d3f22510724752a0b9e
Fajar Riansyah <fajary781@gmail.com>
2024-12-25 21:29:17 +0700

    feat: add multi-file input field component

---
+++ src/components/fields/MultiFileField.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { InputTypeHTMLAttribute } from 'vue';
+import HeadingSix from '../fonts/HeadingSix.vue';
+
+const props = defineProps<{
+  id: string,
+  label: string,
+  placeholder?: string,
+  type?: InputTypeHTMLAttribute,
+}>()
+</script>
+
+<template>
+  <div>
+    <label :for="props.id">
+      <HeadingSix :text="props.label" class="mb-1.5" />
+    </label>
+    <input type="file" multiple
+      class="w-full text-gray-500 border border-gray-300 rounded outline-none file:p-2 focus:outline-primary file:font-semibold file:bg-primary file:border-none file:text-onPrimary bg-gray-50"
+      :id="props.id" :placeholder="props.placeholder ?? props.label">
+  </div>
+</template>
