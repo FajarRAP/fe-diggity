f48e17f50bf59d6e59e093eb4ad20306ad48f8af
Fajar Riansyah <fajary781@gmail.com>
2024-12-25 21:28:34 +0700

    refactor (breadcumb): add optional parameter to

--- src/components/breadcumb/Breadcumb.vue
+++ src/components/breadcumb/Breadcumb.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import RoutesName from '@/router/routes';
 import Home from '../icons/Home.vue'
+
+const props = defineProps<{
+  to?: string
+}>()
 </script>
 
 <template>
   <div class="flex items-center xl:gap-7 lg:gap-5">
-    <RouterLink :to="RoutesName.serviceRoute">
+    <RouterLink :to="to ?? RoutesName.serviceRoute">
       <Home />
     </RouterLink>
     <slot></slot>
